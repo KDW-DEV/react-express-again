bd43c5c3f1f587be9e10598029dcd30a4130b625
Tristan <tristancc99@gmail.com>
2020-04-04 14:15:22 -0400

    ed

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express()
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/public")))
+app.use(express.static(path.join(__dirname, "client/src")))
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
