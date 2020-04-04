024d4d9d0ce45f52c3ee39f52fe496eea7f4a871
Tristan <tristancc99@gmail.com>
2020-04-04 14:29:45 -0400

    nofixy

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "client/build", "index.html"))
   })
 }
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 app.listen(port)
 
 console.log(`Server listening on ${port}`)
