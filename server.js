0b9dc0964757fc857fc017d03fec6ff42c630c79
Tristan <tristancc99@gmail.com>
2020-04-04 14:32:11 -0400

    fixyforreal

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const path = require("path")
 const app = express()
 
 // Serve static files from the React app
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")))
-  // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"))
-  })
-}
+
+// Serve any static files
+app.use(express.static(path.join(__dirname, "client/build")))
+// Handle React routing, return all requests to React app
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"))
+})
+
 const port = process.env.PORT || 5000
 app.listen(port)
 
