3dfb85d164d59bd4829cd2b85ae7a073b4728d69
Tristan <tristancc99@gmail.com>
2020-04-04 14:19:39 -0400

    guh

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, "client/src")))
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/public/index.html"))
+  res.sendFile(path.join(__dirname + "/client/build/index.html"))
 })
 
 const port = process.env.PORT || 3000
