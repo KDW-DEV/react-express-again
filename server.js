5aed6ebceca4e69a9be836515c64fe02ff1d9f4e
Tristan <tristancc99@gmail.com>
2020-04-04 14:25:24 -0400

    fixy

--- server.js
+++ server.js
@@ -4,15 +4,14 @@ const path = require("path")
 const app = express()
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/public")))
-app.use(express.static(path.join(__dirname, "client/src")))
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"))
-})
-
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")))
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"))
+  })
+}
 const port = process.env.PORT || 3000
 app.listen(port)
 
