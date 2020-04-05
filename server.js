4c97bd702700a9e8dc0a61b68b22987ea020cfe3
Tristan <tristancc99@gmail.com>
2020-04-04 20:20:08 -0400

    chicken

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express()
 // Serve any static files
 app.use(express.static(path.join(__dirname, "client/build")))
 // Handle React routing, return all requests to React app
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"))
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"))
 })
 
 const port = process.env.PORT || 5000
